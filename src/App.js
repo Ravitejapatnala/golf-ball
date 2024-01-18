0be14dc21640f739bf2c020bf011a1ce571a60f8
akhil <akhil.ce08@gmail.com>
2024-01-18 14:49:04 +0530

    first commit and uploaded files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+
+  const[leftPosition, setLeftPosition]= useState(0)
+
+  function handleBall(e){
+    if(e.key==='ArrowRight')
+    {
+      setLeftPosition(leftPosition+5)
+    }
+  }
+
+  const[showBall, setShowBall]= useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" onKeyDown={handleBall}>
+      <button onClick={()=>setShowBall(!showBall)}>{showBall? <div style={{"position": "absolute", "left": `${leftPosition}px`}} className="ball"></div> :"start"}</button>
     </div>
-  );
+  )
 }
 
 export default App;
